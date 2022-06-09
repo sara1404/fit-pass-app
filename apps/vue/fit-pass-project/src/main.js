6e4fcc1a10429ec51ce9821a5083fd660546ebda
sara1404 <sara14042000@gmail.com>
2022-06-09 23:07:44 +0200

    user profile header component created

--- apps/vue/fit-pass-project/src/main.js
+++ apps/vue/fit-pass-project/src/main.js
@@ -1,11 +1,13 @@
 import * as Vue from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
 
 
 const app = Vue.createApp(App)
-
+app.use(VueAxios, axios)
 app.use(createPinia())
 app.use(router)
 
