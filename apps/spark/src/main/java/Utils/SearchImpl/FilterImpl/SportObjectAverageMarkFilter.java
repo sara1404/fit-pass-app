422ec68c5c2e8221d3a782379f59b47885a878a5
Srdjan Stjepanovic <businessrdjan@gmail.com>
2022-06-20 00:49:04 +0200

    Updated objects example

--- apps/spark/src/main/java/Utils/SearchImpl/FilterImpl/SportObjectAverageMarkFilter.java
+++ apps/spark/src/main/java/Utils/SearchImpl/FilterImpl/SportObjectAverageMarkFilter.java
@@ -10,13 +10,10 @@ import java.util.Map;
 public class SportObjectAverageMarkFilter implements IFilter<SportObject> {
     @Override
     public List<SportObject> filter(List<SportObject> objects, Map<String, String[]> params) {
-        System.out.println("working");
         if(!params.containsKey("averageMark") || !isValidInput(params.get("averageMark")[0])){
-            System.out.println("No average mark");
             return objects;
         }
         String filteredInput = params.get("averageMark")[0].trim();
-
         return matchAverageMarkCase(objects, filteredInput);
     }
 
