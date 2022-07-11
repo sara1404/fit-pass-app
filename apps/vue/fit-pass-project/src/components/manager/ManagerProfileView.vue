5e4564d8cc54aae7950db2e61caa228d1ae6bbc4
sara1404 <sara14042000@gmail.com>
2022-07-12 00:35:10 +0200

    manager profile view style updated

--- apps/vue/fit-pass-project/src/components/manager/ManagerProfileView.vue
+++ apps/vue/fit-pass-project/src/components/manager/ManagerProfileView.vue
@@ -22,8 +22,11 @@ export default {
 
 <style scoped>
 .profile-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     max-width: 100%;
-    min-height: 100%;
+    min-height: 80vh;
     height: 100%;
     padding: 1.5em;
 }
